109b80ab7b7d0fae8634458fff07d9c6b1f2dce4
bradchao <bk400288>
2020-07-18 15:38:38 +0800

    071807

--- app/src/main/java/tw/brad/apps/listviewtest/DetailActivity.java
+++ app/src/main/java/tw/brad/apps/listviewtest/DetailActivity.java
@@ -3,18 +3,27 @@ package tw.brad.apps.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
+
 public class DetailActivity extends AppCompatActivity {
     private ImageView img;
     private TextView org, place;
+    private RequestQueue queue;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        queue = Volley.newRequestQueue(this);
 
         Intent intent = getIntent();
 
@@ -24,6 +33,23 @@ public class DetailActivity extends AppCompatActivity {
 
         org.setText(intent.getStringExtra("org"));
         place.setText(intent.getStringExtra("place"));
+        url = intent.getStringExtra("img");
+        fetchImage();
+    }
 
+    private void fetchImage(){
+        ImageRequest request = new ImageRequest(
+                url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        img.setImageBitmap(response);
+                    }
+                },
+                0,0,
+                Bitmap.Config.ARGB_8888,
+                null
+        );
+        queue.add(request);
     }
 }
