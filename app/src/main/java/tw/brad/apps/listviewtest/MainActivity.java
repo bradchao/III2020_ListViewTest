e97e6c2fd94edf0febe182520949ba66dbd057db
bradchao <bk400288>
2020-07-18 12:03:03 +0800

    071802

--- app/src/main/java/tw/brad/apps/listviewtest/MainActivity.java
+++ app/src/main/java/tw/brad/apps/listviewtest/MainActivity.java
@@ -3,12 +3,31 @@ package tw.brad.apps.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        initListView();
     }
+
+    private SimpleAdapter adapter;
+    private LinkedList<HashMap<String,String>> data;
+
+    private void initListView(){
+        data = new LinkedList<>();
+        adapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+        listView.setAdapter(adapter);
+    }
+
 }
