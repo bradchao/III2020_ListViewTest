5647f04859a7b88055c45b274cab68850f1fce60
bradchao <bk400288>
2020-07-18 13:58:48 +0800

    071803

--- app/src/main/java/tw/brad/apps/listviewtest/MainActivity.java
+++ app/src/main/java/tw/brad/apps/listviewtest/MainActivity.java
@@ -3,31 +3,59 @@ package tw.brad.apps.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        queue = Volley.newRequestQueue(this);
+
         listView = findViewById(R.id.listView);
         initListView();
+        getGiftData();
     }
 
     private SimpleAdapter adapter;
     private LinkedList<HashMap<String,String>> data;
+    private String[] from = {"title"};
+    private int[] to = {R.id.item_title};
 
     private void initListView(){
         data = new LinkedList<>();
         adapter = new SimpleAdapter(this, data, R.layout.item, from, to);
         listView.setAdapter(adapter);
+        //getGiftData();
+    }
+
+    private void getGiftData(){
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                "https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvAgriculturalProduce.aspx",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("bradlog", response);
+                    }
+                },
+                null);
+        queue.add(request);
     }
 
 }
